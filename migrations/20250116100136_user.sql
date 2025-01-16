c37a6192a2cd36205aca3690f8eaeb94efa11d20
Alexander Kleidinc <kleydints@petalmail.com>
2025-01-16 21:13:59 +0300

    fixed the cargo toml and added random_gen_uuid to the user table

--- migrations/20250116100136_user.sql
+++ migrations/20250116100136_user.sql
@@ -1,6 +1,6 @@
 -- Add migration script here
 CREATE TABLE IF NOT EXISTS "user" (
-    user_id uuid,
+    user_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     first_name numeric(15),
     last_name numeric(25),
     telephone numeric(15)
